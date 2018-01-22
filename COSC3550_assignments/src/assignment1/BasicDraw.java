f3d902c6f210df4ac389f8d6133ab10fbbf48373
dbergqu1745 <daniel.b1745@gmail.com>
2018-01-22 14:16:56 -0600

    finished hw assignment 1 in COSC 3550

--- COSC3550_assignments/src/assignment1/BasicDraw.java
+++ COSC3550_assignments/src/assignment1/BasicDraw.java
@@ -7,10 +7,7 @@ import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
-import javafx.animation.*;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 /*
  * Author: Daniel Bergquist
@@ -19,63 +16,64 @@ import java.util.concurrent.TimeUnit;
  * Basic scene to practice drawing shapes and
  * use different colors
  */
-public class BasicDraw extends Application{
-	
+public class BasicDraw extends Application {
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage stage) {
 		stage.setTitle("Assignment 1");
-		
-		//Creating nodes
+
+		// Creating nodes
 		Group root = new Group();
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
-		
+
 		Canvas canvas = new Canvas(600, 600);
 		root.getChildren().add(canvas);
-		
+
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		render(gc);
-		
+
 		stage.show();
 	}
-	
+
 	void render(GraphicsContext context) {
 		Random rand = new Random();
 		double[] rands = new double[3];
-		int buff = 5;
+		int buff = 10;
 		int length = 600;
-		int xCoordRect = 0;
-		int yCoordRect = 0;
-		int xCoordCircle;
-		int yCoordCircle;
-		
-		
-		for (int i = 300; i >= 0; i--) {
-			
-			//random doubles for making different 
-			//colors with Color.color(dbl, dbl, dbl)
+
+		int coord = 0;
+
+		//for the size of the canvas
+		for (int i = 0; i < 60; i++) {
+
+			/*
+			 * random doubles for making different
+			 * colors with Color.color(dbl, dbl, dbl)
+			 */
 			for (int j = 0; j < 3; j++) {
 				rands[j] = rand.nextDouble();
 			}
-			
+
 			context.setFill(Color.color(rands[0], rands[1], rands[2]));
-			
-			//Alternating square and circle
-			//TODO: finish implementing circle draw part
+
+			// Alternating square and circle
 			if (i % 2 == 0) {
-				context.fillRect(xCoordRect, yCoordRect, length, length);
+				context.fillRect(coord, coord, length, length);
 			} else {
-				//context.fillOval(x, y, w, h);
+				context.fillOval(coord, coord, length, length);
+				
+				/*
+				 * offsetting the coordinates and adjusting the length
+				 * before the next square-circle pair
+				 */
+				coord += buff;
+				length -= 20;
 			}
-			
-			xCoordRect += buff;
-			yCoordRect += buff;
-			length -= 10;
 		}
-		
 	}
 }
